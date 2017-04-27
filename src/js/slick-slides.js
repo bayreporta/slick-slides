b17229d39df087da4b880a2a48476854dd4cac09
bayreporta <bayreporta@gmail.com>
2017-04-26 18:58:13 -0700

    building base SlickSlide class

--- src/js/slick-slides.js
+++ src/js/slick-slides.js
@@ -1,1 +1,43 @@
-console.log('yo');
+/* Parent object for all slide objects - all slides will inherit from this constructor
+==============================================================================================*/
+function SlickSlide( id, prettyid, type, dLeft, dRight, dUp, dDown, speed, options, optTot, optLoc, background, backgroundVal, header, headerVal ) {
+	//init instance properties
+	this.id = id;
+	this.prettyid = prettyid;
+	this.type = type;
+	this.directions = {
+		left: 	dLeft,
+		right: 	dRight,
+		up: 	dUp,
+		down: 	dDown
+	};
+	this.speed = speed;
+	this.background = {
+		type: 	background,
+		value: 	backgroundVal
+	};
+
+	//check if there are options
+	if ( typeof( options ) !== null ){
+		this.options = {
+			total: 		optTot,
+			location: 	optLoc
+		};
+	}
+
+	//check if there is a header
+	if ( typeof( header ) !== null ){ 
+		this.header = headerVal; 
+	}
+
+}
+
+var slide = [];
+	slide[0] = new SlickSlide( 23, 'prettyid', 'full', 22, 24, null, null, 1, false, null, null, 'color', '#000', true, 'Hello World' );
+
+SlickSlide.prototype.getProperty = function(){
+	return this.property;
+}
+
+
+
